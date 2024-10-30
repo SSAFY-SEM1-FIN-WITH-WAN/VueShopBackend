782338f210cb63bc53e3148b0d8f6c58750765bc
lsc713 <lsc7134@naver.com>
2024-10-30 21:14:41 +0900

    init commit

---
+++ src/main/java/com/ssafy/commerce/demo/SellProductApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.commerce.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/commerce/demo/SellProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.commerce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
