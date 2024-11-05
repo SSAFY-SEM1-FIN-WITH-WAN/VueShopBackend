e2f2eccf40682b7eb74e279e33d7bd62abe54f27
lsc713 <lsc7134@naver.com>
2024-11-05 14:04:40 +0900

    feat:add back logic

---
+++ src/main/java/com/ssafy/commerce/demo/config/SwggerConfig.java
@@ -0,0 +1,17 @@
+//package com.ssafy.commerce.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class SwggerConfig {
+//	 @Bean
+//	  public OpenAPI springShopOpenAPI() {
+//	      return new OpenAPI()
+//	              .info(new Info().title("Spring Boot Board REST API")
+//	              .description("게시판 REST API 입니다.")
+//	              .version("v0.0.1")
+//	              .license(new License().name("SSAFY").url("https://www.ssafy.com")));
+//	  }
+//}
---
+++ src/main/java/com/ssafy/commerce/demo/controller/BoardRestController.java
@@ -0,0 +1,128 @@
+package com.ssafy.commerce.demo.controller;
+
+import com.ssafy.commerce.demo.model.service.BoardService;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ssafy.commerce.demo.model.dto.Board;
+import com.ssafy.commerce.demo.model.dto.SearchCondition;
+
+@RestController //@Controller + @ResponseBody
+@RequestMapping("/api-board")
+public class BoardRestController {
+	//서비스 의존성 주입
+	private final BoardService boardService;
+	
+	@Autowired
+	public BoardRestController(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
+	//게시글 전체조회
+//	@GetMapping("/board")
+//	public ResponseEntity<List<Board>> list(){
+//		List<Board> list = boardService.getBoardList();
+//		return new ResponseEntity<>(list, HttpStatus.OK);
+//	}
+	
+	//검색 
+	@GetMapping("/board")
+	public ResponseEntity<?> list(@ModelAttribute SearchCondition condition){
+		System.out.println(condition);
+		List<Board> list = boardService.search(condition);
+		
+		
+		if(list == null || list.size() == 0) {
+			return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<Board>>(list, HttpStatus.OK);
+	}
+	
+	
+	//게시글 상세 보기
+	@GetMapping("/board/{id}")
+	public ResponseEntity<Board> detail(@PathVariable("id") int id){
+		Board board = boardService.readBoard(id);
+//		System.out.println(board);
+		if(board != null) {
+			return ResponseEntity.ok(board);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	//게시글 등록(Form 데이터 형식으로 넘어왔다)
+	@PostMapping("/board")
+	public ResponseEntity<?> write(@ModelAttribute Board board){
+		//게시글 등록 요청
+		boardService.writeBoard(board); 
+		System.out.println(board);
+		
+		int id = board.getId();
+		//입력한 board 자체를 넘겼다. 의미가 있나?
+		//board를 넘기기 보다는... id 같은 것을 넘겨서
+		//프론트에서 detail로 요청을 보내게끔 만들면 좋겠는데.....
+		return new ResponseEntity<Board>(board, HttpStatus.CREATED);
+	}
+	
+	//게시글 삭제
+	@DeleteMapping("/board/{id}")
+	public ResponseEntity<String> delete(@PathVariable("id") int id){
+		boolean isDeleted = boardService.removeBoard(id);
+		
+		if(isDeleted)
+			return ResponseEntity.status(HttpStatus.OK).body("Board deleted");
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed");
+	}
+	
+	//게시글 수정
+//	@PutMapping("/board")
+	@PutMapping("/board/{id}")
+	public ResponseEntity<Void> update(@PathVariable("id") int id, @RequestBody Board board){
+		System.out.println(board);
+		board.setId(id);
+		boardService.modifyBoard(board);
+		//아주 심플하게 무조건 성공! 해놓았지만..... 
+		//세밀하게 컨츄롤 할 수 있는 부분이다.
+		return new ResponseEntity<Void>(HttpStatus.OK); //
+	}
+	
+	
+	//파일업로드
+	@PostMapping("/upload")
+	public ResponseEntity<Void> fileUpload(@RequestParam("file") MultipartFile file, @ModelAttribute Board board){
+//		System.out.println(file.getOriginalFilename());
+//		System.out.println(board);
+		
+		boardService.fileUpload(file, board);
+		
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
