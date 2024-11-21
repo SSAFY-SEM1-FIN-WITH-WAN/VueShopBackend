cd0bc41e2b65147a8715839513a174c7b67a6bf1
lsc7134 <lsc7134@naver.com>
2024-11-21 09:46:43 +0900

    test

--- src/main/java/com/ssafy/commerce/demo/weather/controller/WeatherController.java
+++ src/main/java/com/ssafy/commerce/demo/weather/controller/WeatherController.java
@@ -24,82 +24,50 @@ public class WeatherController {
 	private static final String WEATHER_REQUEST_BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
 	private static final String WEATHER_REQUEST_BASE_URL2 = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
 	public static final String reqTime = String.format("0200");
-    public static final String reqTime2 = String.format("%02d30",LocalDateTime.now().getHour()-2);
-    public static final String DEFAULT_PAGE_NO = "3";
-    public static final String DEFAULT_ROWS = "20";
-    
+	public static final String reqTime2 = String.format("%02d30", LocalDateTime.now().getHour() - 2);
+	public static final String DEFAULT_PAGE_NO = "3";
+	public static final String DEFAULT_ROWS = "20";
+
 	private WeatherService weatherService;
-	
+
 	public WeatherController(WeatherService weatherService) {
 		this.weatherService = weatherService;
 	}
-	
+
 	@GetMapping
 	@TimeTrace
-	public ResponseEntity<?> requestWeather(double longitude, double latitude) throws IOException{
-		WeatherResponseDto dto = weatherService.requestWeather(longitude,latitude,WEATHER_REQUEST_BASE_URL,reqTime,"3","20");
-		List<Item> itemList = dto.getResponse().getBody().getItems().getItem();
-		List<Item> filteredList = new ArrayList<>();
-		for(Item item : itemList) {
-			if(item.getFcstTime().equals(String.format("0600"))&&item.getCategory().equals("TMN")) {
-				filteredList.add(item);
-			}
-		}
-		dto.getResponse().getBody().getItems().setItem(filteredList);
-//		System.out.println("--------------------------------------------");
-		System.out.println(dto.toString());
-		return ResponseEntity.ok(dto);
+	public ResponseEntity<?> requestWeather(double longitude, double latitude) throws IOException {
+		return requestWeatherWithFilter(longitude, latitude, WEATHER_REQUEST_BASE_URL, reqTime, "3", "20", "0600", "TMN");
 	}
-	
-	@TimeTrace
+
 	@GetMapping("/2")
-	public ResponseEntity<?> requestWeather2(double longitude, double latitude) throws IOException{
-		WeatherResponseDto dto = weatherService.requestWeather(longitude,latitude,WEATHER_REQUEST_BASE_URL2,reqTime2,"6","5");
-//		System.out.println(dto.toString());
-		List<Item> itemList = dto.getResponse().getBody().getItems().getItem();
-		List<Item> filteredList = new ArrayList<>();
-		for(Item item : itemList) {
-			if(item.getFcstTime().equals(String.format("%02d00",LocalDateTime.now().getHour()))) {
-				filteredList.add(item);
-			}
-				
-		}
-		dto.getResponse().getBody().getItems().setItem(filteredList);
-		return ResponseEntity.ok(dto);
-	}
-	
 	@TimeTrace
+	public ResponseEntity<?> requestWeather2(double longitude, double latitude) throws IOException {
+		return requestWeatherWithFilter(longitude, latitude, WEATHER_REQUEST_BASE_URL2, reqTime2, "6", "5", String.format("%02d00", LocalDateTime.now().getHour()), null);
+	}
+
 	@GetMapping("/3")
-	public ResponseEntity<?> requestWeather3(double longitude, double latitude) throws IOException{
-		WeatherResponseDto dto = weatherService.requestWeather(longitude,latitude,WEATHER_REQUEST_BASE_URL,reqTime,"8","20");
-//		System.out.println(dto.toString());
-		List<Item> itemList = dto.getResponse().getBody().getItems().getItem();
-		List<Item> filteredList = new ArrayList<>();
-		for(Item item : itemList) {
-			if(item.getFcstTime().equals(String.format("1500"))&&item.getCategory().equals("TMX")) {
-				filteredList.add(item);
-			}
-		}
-		dto.getResponse().getBody().getItems().setItem(filteredList);
-		return ResponseEntity.ok(dto);
-	}
-	
 	@TimeTrace
+	public ResponseEntity<?> requestWeather3(double longitude, double latitude) throws IOException {
+		return requestWeatherWithFilter(longitude, latitude, WEATHER_REQUEST_BASE_URL, reqTime, "8", "20", "1500", "TMX");
+	}
+
 	@GetMapping("/4")
-	public ResponseEntity<?> requestWeather4(double longitude, double latitude) throws IOException{
-		WeatherResponseDto dto = weatherService.requestWeather(longitude,latitude,WEATHER_REQUEST_BASE_URL2,reqTime2,"1","500");
-//		System.out.println(dto.toString());
+	@TimeTrace
+	public ResponseEntity<?> requestWeather4(double longitude, double latitude) throws IOException {
+		return requestWeatherWithFilter(longitude, latitude, WEATHER_REQUEST_BASE_URL2, reqTime2, "1", "500", String.format("%02d00", LocalDateTime.now().getHour()), "PTY");
+	}
+
+	private ResponseEntity<?> requestWeatherWithFilter(double longitude, double latitude, String baseUrl, String reqTime, String pageNo, String numOfRows, String fcstTime, String category) throws IOException {
+		WeatherResponseDto dto = weatherService.requestWeather(longitude, latitude, baseUrl, reqTime, pageNo, numOfRows);
 		List<Item> itemList = dto.getResponse().getBody().getItems().getItem();
 		List<Item> filteredList = new ArrayList<>();
-		for(Item item : itemList) {
-			if(item.getFcstTime().equals(String.format("%02d00",LocalDateTime.now().getHour()))&&item.getCategory().equals("PTY")) {
+		for (Item item : itemList) {
+			if (item.getFcstTime().equals(fcstTime) && (category == null || item.getCategory().equals(category))) {
 				filteredList.add(item);
 			}
-				
 		}
 		dto.getResponse().getBody().getItems().setItem(filteredList);
-//		System.out.println("--------------------------------------------");
-//		System.out.println(dto.toString());
 		return ResponseEntity.ok(dto);
 	}
 }
